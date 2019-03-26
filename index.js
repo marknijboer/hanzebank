3b44da01f57231d881c2af8d91f3f93e992cbe73
resdac <mathijswesterhof@outlook.com>
2019-03-27 00:18:38 +0100

    setting up twig support

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const routes = require("./lib/routes.js");
 const middleware = require("./lib/middleware.js");
+const path = require('path');
 const app = express();
 
 // Environment variable PORT or else port 3000
@@ -12,8 +13,12 @@ middleware(app);
 // Add routes
 routes(app);
 
+app.set('views', path.join(__dirname, 'lib/templates'));
+app.set('view engine', 'twig');
+app.use(express.static(path.join(__dirname, 'lib/public')))
+
 // Synchronize database
 require("./lib/models/sync.js");
 
 // Start the app on the given port
-app.listen(port, () => console.log(`Hanze banking app listening on port ${port}!`))
+app.listen(port, () => console.log(`Hanze banking app listening on port ${port}!`));
